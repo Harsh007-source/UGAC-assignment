9d1ac6369387a73e7ef31327e00651f3db03e040
Harshvardhan Parihar <pariharharsh2006@gmail.com>
2025-06-30 22:38:09 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,16 +1,16 @@
 'use client';
 import { useEffect, useState } from 'react';
-import { getTasks, deleteTask } from '../utils/storage';
-import Link from 'next/link';
+import { getTasks, deleteTask } from '../utils/storage';  //getTasks func to extract the list of saved tasks from localStorage
+import Link from 'next/link';  // navigate between pages without reloading the page
 
 export default function Dashboard() {
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState([]); //'task' initialized as empty array
 
   useEffect(() => {
     setTasks(getTasks());
   }, []);
 
-  const handleDelete = (id) => {
+  const handleDelete = (id) => {  // it is called when user clicks the delete button for a task
     deleteTask(id);
     setTasks(getTasks());
   };
@@ -18,18 +18,18 @@ export default function Dashboard() {
   return (
     <div>
       <h1>Task Dashboard</h1>
-      <Link href="/create">+ Create Task</Link>
-      <ul>
+      <Link href="/create">+ Create Task</Link>  
+      <ul>  // unordered list to display all tasks as list items
         {tasks.map((task) => (
-          <li key={task.id}>
-            <strong>{task.title}</strong> - {task.status} - {task.priority}
+          <li key={task.id}>    // display the task in bold along with its status and priority
+            <strong>{task.title}</strong> - {task.status} - {task.priority}   
             <br />
             Due: {task.dueDate}
             <br />
             Tags: {task.tags}
             <br />
-            <Link href={`/task/${task.id}`}>Edit</Link>
-            <button onClick={() => handleDelete(task.id)}>Delete</button>
+            <Link href={`/task/${task.id}`}>Edit</Link>  
+            <button onClick={() => handleDelete(task.id)}>Delete</button>  
           </li>
         ))}
       </ul>
